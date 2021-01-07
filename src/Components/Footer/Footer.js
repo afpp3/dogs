213275b2adb8eaa9ebd17e1a2109030295a98d75
André Felippe <afelipp3@gmail.com>
2021-01-07 19:10:10 -0300

    Create Feed Page

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,10 @@
-import React from "react";
-import styles from "./Footer.module.css";
+import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h1>Footer</h1>
+      <h1></h1>
     </footer>
   );
 };
