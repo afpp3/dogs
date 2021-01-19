9aa63c5128fe8cd2d643a012f7714726c8ab632b
André Felippe <afelipp3@gmail.com>
2021-01-19 19:17:43 -0300

    Add PropTypes on Feed

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
             <Route path="login/*" element={<Login />} />
             <ProtectedRoute path="conta/*" element={<User />} />
             <Route path="photo/:id" element={<Photo />} />
-            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="profile/:user" element={<UserProfile />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
